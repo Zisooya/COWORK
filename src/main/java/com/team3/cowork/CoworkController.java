793b5025bdbf84e0306f50d92d9dfe6aea6ac2f7
YJ-Nam <107850856+dltprjs1@users.noreply.github.com>
2022-12-08 17:46:22 +0900

    [STYLE] 프로젝트 BOARD 추가

--- src/main/java/com/team3/cowork/CoworkController.java
+++ src/main/java/com/team3/cowork/CoworkController.java
@@ -29,7 +29,7 @@ public class CoworkController {
 
 	@Autowired
 	private MemberService service;
-  
+
 	// ProjectDAO 변수 생성 _ 세건
 	@Autowired
 	private ProjectsDAO dao_projects;
@@ -40,170 +40,186 @@ public class CoworkController {
 		List<com.team3.model.ProjectsDTO> list = this.dao_projects.getProjectsList();
 		List<Main_ProjectsDTO> main = this.dao_projects.getMainList();
 		List<Projects_statusDTO> status = this.dao_projects.getStatusList();
-	    List<MemberDTO> mlist = this.dao.getMemberList();
-	    model.addAttribute("mlist", mlist);
+		List<MemberDTO> mlist = this.dao.getMemberList();
+		model.addAttribute("mlist", mlist);
 		model.addAttribute("list", list);
 		model.addAttribute("main", main);
 		model.addAttribute("status", status);
 		return "project_control";
 	}
-			
 
-  // 프로젝트 생성 페이지 _ 세건
-  @RequestMapping("project_insert.do")
-  public void project_insert(Model model,ProjectsDTO dto,HttpServletResponse response,MemberDTO mdto) throws IOException {
-    this.dao_projects.insertProject(dto);
-    List<MemberDTO> mlist = this.dao.getMemberList();
-    model.addAttribute("mlist", mlist);
-    response.setContentType("text/html; charset=UTF-8");
-    PrintWriter out = response.getWriter();
-    out.println("<script>");
-    out.println("location.href='project_control.do'");
-    out.println("</script>");
-  }
+	// 프로젝트 생성 페이지 _ 세건
+	@RequestMapping("project_insert.do")
+	public void project_insert(Model model, ProjectsDTO dto, HttpServletResponse response, MemberDTO mdto)
+			throws IOException {
+		this.dao_projects.insertProject(dto);
+		List<MemberDTO> mlist = this.dao.getMemberList();
+		model.addAttribute("mlist", mlist);
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println("<script>");
+		out.println("location.href='project_control.do'");
+		out.println("</script>");
+	}
 
-  // 프로젝트 상세보기 모달창 띄우기 _ 세건
-  @RequestMapping("content.do")
-  public String ProjectModal(Model model,@RequestParam int num) throws IOException {
-    ProjectsDTO cont = this.dao_projects.getprojects(num);
-    List<Main_ProjectsDTO> main = this.dao_projects.getMainList();
-    List<MemberDTO> mlist = this.dao.getMemberList();
-    List<Projects_statusDTO> status = this.dao_projects.getStatusList();
-    model.addAttribute("mlist", mlist);
-    model.addAttribute("cont", cont);
-    model.addAttribute("main", main);
-    model.addAttribute("status", status);
-    return "projects_include/Project_modal";
-    }
-  
-  // 프로젝트 멤버 추가하기 _ 세건
-  @ResponseBody
-  @RequestMapping("project_memberinsert.do")
-  public Map<String, String> ProjectMemberInsert(ProjectsDTO dto,HttpServletResponse response) throws IOException{
-	  int check = 0;
-	  String taker = "";
-	  ProjectsDTO cont = this.dao_projects.getprojects(dto.getProject_no());
-	  
-	  if(cont.getProject_taker() == null){
-		  check = this.dao_projects.updatetaker(dto);
-		  taker = dto.getProject_taker();
-	  }else if(cont.getProject_taker2() == null) {
-		  check = this.dao_projects.updatetaker2(dto);
-		  taker = dto.getProject_taker2();
-	  }else if(cont.getProject_taker3() == null) {
-		  check = this.dao_projects.updatetaker3(dto);
-		  taker = dto.getProject_taker3();
-	  }else if(cont.getProject_taker4() == null) {
-		  check = this.dao_projects.updatetaker4(dto);
-		  taker = dto.getProject_taker4();
-	  }else if(cont.getProject_taker5() == null) {
-		  check = this.dao_projects.updatetaker5(dto);
-		  taker = dto.getProject_taker5();
-	  }
-	  Map<String, String> map = new HashMap<String,String>(); 
-      
-      map.put("dog","강아지");
-      map.put("cat","고양이");
-      // ex_3
-      // list에 id, pw, content를 키값으로 값이 담겨있음
-      // {"id":"admin"},{"pw":"1234"},{"content":"내용 입니다."}
-      
-	return map; // Ajax로 넘겨줄 값 text, map, list
-  }
-  
-  // 프로젝트 멤버 제외하기 _ 세건
-  @RequestMapping("project_Deletetaker1.do")
-  public void projectDeletetaker1(ProjectsDTO dto,HttpServletResponse response) {
-	 int check = this.dao_projects.deletetaker1(dto);
-  }
-  @RequestMapping("project_Deletetaker2.do")
-  public void projectDeletetaker2(ProjectsDTO dto,HttpServletResponse response) {
-	 int check = this.dao_projects.deletetaker2(dto);
-  }
-  @RequestMapping("project_Deletetaker3.do")
-  public void projectDeletetaker3(ProjectsDTO dto,HttpServletResponse response) {
-	 int check = this.dao_projects.deletetaker3(dto);
-  }
-  @RequestMapping("project_Deletetaker4.do")
-  public void projectDeletetaker4(ProjectsDTO dto,HttpServletResponse response) {
-	 int check = this.dao_projects.deletetaker4(dto);
-  }
-  @RequestMapping("project_Deletetaker5.do")
-  public void projectDeletetaker5(ProjectsDTO dto,HttpServletResponse response) {
-	 int check = this.dao_projects.deletetaker5(dto);
-  }
-  
-  // 프로젝트 삭제하기 _ 세건
-  @RequestMapping("project_delete.do")
-  public void ProjectDelete(@RequestParam int num, HttpServletResponse response) throws IOException {
-	  System.out.println(num);
-	  int check = this.dao_projects.deleteProjects(num);
-	  response.setContentType("text/html; charset=UTF-8");
-	  PrintWriter out = response.getWriter();
-	  if(check > 0) {
-	  out.println("<script>");
-	  out.println("location.href='project_control.do'");
-	  out.println("</script>");
-	  }
-  }
-  
-  // 프로젝트 status 변경 _ 세건
-  @RequestMapping("project_move.do")
-  public void ProjectMove(ProjectsDTO dto,HttpServletResponse response) throws IOException {
-	  int check = this.dao_projects.updateProjects(dto);
-	  response.setContentType("text/html; charset=UTF-8");
-	  PrintWriter out = response.getWriter();
-	  if(check > 0) {
-	  out.println("<script>");
-	  out.println("alert('변경되었습니다.')");
-	  out.println("</script>");
-	  }
-  }
-  
-  // 프로젝트 시작일 변경 _ 세건
-  @RequestMapping("project_UpdateStart.do")
-  public void ProjectUpdateStart(ProjectsDTO dto,HttpServletResponse response) throws IOException {
-	  int check = this.dao_projects.updatestart(dto);
-	  response.setContentType("text/html; charset=UTF-8");
-	  PrintWriter out = response.getWriter();
-	  if(check > 0) {
-		  out.println("<script>");
-		  out.println("alert('변경되었습니다.')");
-		  out.println("</script>");
-	  }
-  }
-  
-  // 프로젝트 마감일 변경 _ 세건
-  @RequestMapping("project_Updateend.do")
-  public void ProjectUpdateEnd(ProjectsDTO dto,HttpServletResponse response) throws IOException {
-	  int check = this.dao_projects.updateend(dto);
-	  response.setContentType("text/html; charset=UTF-8");
-	  PrintWriter out = response.getWriter();
-	  if(check > 0) {
-		  out.println("<script>");
-		  out.println("alert('변경되었습니다.')");
-		  out.println("</script>");
-	  }
-  }
-  
-  // 프로젝트 상세내용 추가하기 _ 세건
-  @RequestMapping("project_InsertComment.do")
-  public void ProjectComment(ProjectsDTO dto,HttpServletResponse response) throws IOException {
-	  this.dao_projects.updatecomment(dto);
-	  System.out.println(dto.getProject_comment());
-	  System.out.println(dto.getProject_no());
-  }
-  
+	// 프로젝트 상세보기 모달창 띄우기 _ 세건
+	@RequestMapping("content.do")
+	public String ProjectModal(Model model, @RequestParam int num) throws IOException {
+		ProjectsDTO cont = this.dao_projects.getprojects(num);
+		List<Main_ProjectsDTO> main = this.dao_projects.getMainList();
+		List<MemberDTO> mlist = this.dao.getMemberList();
+		List<Projects_statusDTO> status = this.dao_projects.getStatusList();
+		model.addAttribute("mlist", mlist);
+		model.addAttribute("cont", cont);
+		model.addAttribute("main", main);
+		model.addAttribute("status", status);
+		return "projects_include/Project_modal";
+	}
 
+	// 프로젝트 멤버 추가하기 _ 세건
+	@ResponseBody
+	@RequestMapping("project_memberinsert.do")
+	public Map<String, String> ProjectMemberInsert(ProjectsDTO dto, HttpServletResponse response) throws IOException {
+		int check = 0;
+		String taker = "";
+		ProjectsDTO cont = this.dao_projects.getprojects(dto.getProject_no());
+		List<MemberDTO> mlist = this.dao.getMemberList();
+		if (cont.getProject_taker() == null) {
+			check = this.dao_projects.updatetaker(dto);
+			taker = dto.getProject_taker();
+		} else if (cont.getProject_taker2() == null) {
+			check = this.dao_projects.updatetaker2(dto);
+			taker = dto.getProject_taker2();
+		} else if (cont.getProject_taker3() == null) {
+			check = this.dao_projects.updatetaker3(dto);
+			taker = dto.getProject_taker3();
+		} else if (cont.getProject_taker4() == null) {
+			check = this.dao_projects.updatetaker4(dto);
+			taker = dto.getProject_taker4();
+		} else if (cont.getProject_taker5() == null) {
+			check = this.dao_projects.updatetaker5(dto);
+			taker = dto.getProject_taker5();
+		}
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("taker", taker);
+		map.put("cat", "고양이");
+		System.out.println(taker);
+		return map;
+	}
+
+	// 프로젝트 멤버 제외하기 _ 세건
+	@RequestMapping("project_Deletetaker1.do")
+	public void projectDeletetaker1(ProjectsDTO dto, HttpServletResponse response) {
+		int check = this.dao_projects.deletetaker1(dto);
+	}
+
+	@RequestMapping("project_Deletetaker2.do")
+	public void projectDeletetaker2(ProjectsDTO dto, HttpServletResponse response) {
+		int check = this.dao_projects.deletetaker2(dto);
+	}
+
+	@RequestMapping("project_Deletetaker3.do")
+	public void projectDeletetaker3(ProjectsDTO dto, HttpServletResponse response) {
+		int check = this.dao_projects.deletetaker3(dto);
+	}
+
+	@RequestMapping("project_Deletetaker4.do")
+	public void projectDeletetaker4(ProjectsDTO dto, HttpServletResponse response) {
+		int check = this.dao_projects.deletetaker4(dto);
+	}
+
+	@RequestMapping("project_Deletetaker5.do")
+	public void projectDeletetaker5(ProjectsDTO dto, HttpServletResponse response) {
+		int check = this.dao_projects.deletetaker5(dto);
+	}
+
+	// 프로젝트 삭제하기 _ 세건
+	@RequestMapping("project_delete.do")
+	public void ProjectDelete(@RequestParam int num, HttpServletResponse response) throws IOException {
+		System.out.println(num);
+		int check = this.dao_projects.deleteProjects(num);
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		if (check > 0) {
+			out.println("<script>");
+			out.println("location.href='project_control.do'");
+			out.println("</script>");
+		}
+	}
+
+	// 프로젝트 status 변경 _ 세건
+	@RequestMapping("project_move.do")
+	public void ProjectMove(ProjectsDTO dto, HttpServletResponse response) throws IOException {
+		int check = this.dao_projects.updateProjects(dto);
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		if (check > 0) {
+			out.println("<script>");
+			out.println("alert('변경되었습니다.')");
+			out.println("</script>");
+		}
+	}
+
+	// 프로젝트 시작일 변경 _ 세건
+	@RequestMapping("project_UpdateStart.do")
+	public void ProjectUpdateStart(ProjectsDTO dto, HttpServletResponse response) throws IOException {
+		int check = this.dao_projects.updatestart(dto);
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		if (check > 0) {
+			out.println("<script>");
+			out.println("alert('변경되었습니다.')");
+			out.println("</script>");
+		}
+	}
+
+	// 프로젝트 마감일 변경 _ 세건
+	@RequestMapping("project_Updateend.do")
+	public void ProjectUpdateEnd(ProjectsDTO dto, HttpServletResponse response) throws IOException {
+		int check = this.dao_projects.updateend(dto);
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		if (check > 0) {
+			out.println("<script>");
+			out.println("alert('변경되었습니다.')");
+			out.println("</script>");
+		}
+	}
+
+	// 프로젝트 상세내용 추가하기 _ 세건
+	@RequestMapping("project_InsertComment.do")
+	public void ProjectComment(ProjectsDTO dto, HttpServletResponse response) throws IOException {
+		this.dao_projects.updatecomment(dto);
+		System.out.println(dto.getProject_comment());
+		System.out.println(dto.getProject_no());
+	}
+
+	
+	 
+	 // 프로젝트 보드 보기 _ 세건
+	 
+	 @RequestMapping("project_board.do")
+	 public String projectboard(Model model) {
+		 
+	 List<com.team3.model.ProjectsDTO> list = this.dao_projects.getProjectsList();
+	 List<Main_ProjectsDTO> main = this.dao_projects.getMainList();
+	 List<Projects_statusDTO> status = this.dao_projects.getStatusList();
+	 List<MemberDTO> mlist = this.dao.getMemberList(); model.addAttribute("mlist",
+	 mlist); model.addAttribute("list", list); model.addAttribute("main", main);
+	 model.addAttribute("status", status); 
+	 return "projects_include/project_board"; 
+	 
+	 }
+	
 	@Autowired
 	private CalendarDAO dao_cal;
-	
+
 	@RequestMapping("calendar.do")
 	public String calendarMain(Model model) {
-		 
+
 		return "cal_main";
 	}
-	
+
 	@RequestMapping("calendarList.do")
 	@ResponseBody
 	public List<CalendarDTO> calendarList(@RequestParam("no") int memNo, Model model) {
@@ -211,8 +227,7 @@ public class CoworkController {
 		return list;
 	}
 
-
-	@RequestMapping("member_login.do")	// 임시로 만든 메서드임. 추후 로그인 화면을 시작페이지(main.jsp)로 변경 예정.
+	@RequestMapping("member_login.do") // 임시로 만든 메서드임. 추후 로그인 화면을 시작페이지(main.jsp)로 변경 예정.
 	public String login() {
 		return "login";
 	}
@@ -245,9 +260,9 @@ public class CoworkController {
 
 		return mav;
 	}
-	
+
 	@RequestMapping("address.do")
-	public String address(){
+	public String address() {
 		return "address";
 	}
 
