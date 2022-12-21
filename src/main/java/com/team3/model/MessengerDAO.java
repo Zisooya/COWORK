8e7f90157924cd9d4efc4c382c6c2ae608a0da49
Zisooya <113344769+Zisooya@users.noreply.github.com>
2022-12-22 03:20:51 +0900

    include 날아간 거 복구
    * # [ADD]CSS파일 추가  [UPDATE]main.jsp 수정[STYLE]CSS include 파일 추가
    
    * [ADD]이미지 파일 추가 [UPDATE]includ,main작업 [REMOVE]test용 페이지 제거 [STYLE]include.css 작성중
    
    * [STYLE]include파일 수정 [UPDATE]include 페이지 수정
    
    * [ADD]주소록 파일 생성 [UPDATE]include 파일 수정
    
    * [UPDATE] 주소록 사이드 아코디언 메뉴, 모달창 추가
    
    [UPDATE] 주소록 사이드 아코디언 메뉴, 모달창 추가
    
    * [UPDATE]include,address 수정 [CSS]include 페이지 이동 시 정렬 깨지는 것 수정
    
    [UPDATE]include,address 수정 [CSS]include 페이지 이동 시 정렬 깨지는 것 수정
    
    * [UPDATE] 주소록 구성원 초대 추가
    
    * [ADD] 주소록 관련 DTO,DAO 추가 [UPDATE] 조직도 부서 조회
    
    * [REFACTOR] Controller, xml 따로 빼기
    
    * [UPDATE] 부서 별 주소록 조회 AJAX 기능추가
    
    * [UPDATE] 주소록 부서별 조회 기능 완성
    
    * [UPDATE]주소록 검색 기능 추가
    
    * [UPDATE]VIP 체크 기능 추가, 연락처 추가 기능 완료
    
    * [ADD]메신저 관련 파일 생성
    
    * 정상구동 중간 커밋
    
    * [ADD]메신저 관련 파일 생성
    
    * [UPDATE]메신저 채팅목록 조회 기능 추가
    
    * [STYLE] 인클루드 파일 수정
    
    * [UPDATE]채팅 중간 커밋
    
    * 정상구동 커밋
    
    * 중간커밋
    
    * [update] 채팅방 별 메세지 불러오기
    
    * include 날아간 거 복구

--- src/main/java/com/team3/model/MessengerDAO.java
+++ src/main/java/com/team3/model/MessengerDAO.java
@@ -7,4 +7,8 @@ public interface MessengerDAO {
 	List<Chat_RoomDTO> getOneToOneChatRoomList(int mem_no);
 	
 	List<Chat_RoomDTO> getGroupChatRoomList(int mem_no);
+	
+	List<Chat_MessageDTO> getMessageList(int chat_room_no);
+	
+	int messenger_insertMessage(Chat_MessageDTO chatMessageDTO);
 }
